0abdb0ebf511fc7eb2a893afcdfea030a04b79b1
flimberger <florian.limberger@eyeson.team>
2025-07-10 12:08:40 +0200

    Support h265-codec via cmd-line switch

--- examples/rtsp-client/go.mod
+++ examples/rtsp-client/go.mod
@@ -1,19 +1,22 @@
 module rtsp-client
 
-go 1.18
+go 1.23.0
+
+toolchain go1.24.4
 
 require (
-	github.com/bluenviron/gortsplib/v4 v4.10.1
+	github.com/bluenviron/gortsplib/v4 v4.15.0
 	github.com/bluenviron/mediacommon v1.11.1-0.20240525122142-20163863aa75
 	github.com/eyeson-team/eyeson-go v1.7.1-0.20250625151357-066c5249b9be
-	github.com/eyeson-team/ghost/v2 v2.7.1-0.20250626063411-6535290faea3
-	github.com/pion/rtp v1.8.7-0.20240429002300-bc5124c9d0d0
+	github.com/eyeson-team/ghost/v2 v2.7.2-0.20250709135843-59f942bf7001
+	github.com/pion/rtp v1.8.20
 	github.com/rs/zerolog v1.33.0
 	github.com/spf13/cobra v1.3.0
 )
 
 require (
 	github.com/bgentry/actioncable-go v0.0.0-20170309201021-1f2dbd93dbae // indirect
+	github.com/bluenviron/mediacommon/v2 v2.3.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/eyeson-team/gosepp/v3 v3.4.2-0.20250625152754-7520ae68db73 // indirect
 	github.com/google/uuid v1.6.0 // indirect
@@ -26,22 +29,24 @@ require (
 	github.com/pion/dtls/v2 v2.2.10 // indirect
 	github.com/pion/ice/v2 v2.3.14 // indirect
 	github.com/pion/interceptor v0.1.27 // indirect
-	github.com/pion/logging v0.2.2 // indirect
+	github.com/pion/logging v0.2.3 // indirect
 	github.com/pion/mdns v0.0.12 // indirect
 	github.com/pion/randutil v0.1.0 // indirect
-	github.com/pion/rtcp v1.2.14 // indirect
+	github.com/pion/rtcp v1.2.15 // indirect
 	github.com/pion/sctp v1.8.15 // indirect
-	github.com/pion/sdp/v3 v3.0.9 // indirect
+	github.com/pion/sdp/v3 v3.0.14 // indirect
 	github.com/pion/srtp/v2 v2.0.18 // indirect
+	github.com/pion/srtp/v3 v3.0.6 // indirect
 	github.com/pion/stun v0.6.1 // indirect
 	github.com/pion/transport/v2 v2.2.4 // indirect
+	github.com/pion/transport/v3 v3.0.7 // indirect
 	github.com/pion/turn/v2 v2.1.5 // indirect
 	github.com/pion/webrtc/v3 v3.2.34 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
-	github.com/stretchr/testify v1.9.0 // indirect
-	golang.org/x/crypto v0.24.0 // indirect
-	golang.org/x/net v0.26.0 // indirect
-	golang.org/x/sys v0.21.0 // indirect
+	github.com/stretchr/testify v1.10.0 // indirect
+	golang.org/x/crypto v0.39.0 // indirect
+	golang.org/x/net v0.41.0 // indirect
+	golang.org/x/sys v0.33.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- examples/rtsp-client/main.go
+++ examples/rtsp-client/main.go
@@ -15,10 +15,13 @@ import (
 
 	"github.com/bluenviron/gortsplib/v4"
 	"github.com/bluenviron/gortsplib/v4/pkg/base"
+	"github.com/bluenviron/gortsplib/v4/pkg/description"
 	"github.com/bluenviron/gortsplib/v4/pkg/format"
 	"github.com/bluenviron/gortsplib/v4/pkg/format/rtph264"
+	"github.com/bluenviron/gortsplib/v4/pkg/format/rtph265"
 
 	rtsph264 "github.com/bluenviron/mediacommon/pkg/codecs/h264"
+	rtsph265 "github.com/bluenviron/mediacommon/pkg/codecs/h265"
 	"github.com/eyeson-team/eyeson-go"
 	ghost "github.com/eyeson-team/ghost/v2"
 	"github.com/pion/rtp"
@@ -37,6 +40,7 @@ var (
 	quietFlag              bool
 	customCAFileFlag       string
 	insecureSkipVerifyFlag bool
+	useH265CodecFlag       bool
 
 	rootCommand = &cobra.Command{
 		Use:   "rtsp-client [flags] $API_KEY|$GUEST_LINK RTSP_CONNECT_URL",
@@ -121,6 +125,7 @@ func main() {
 	rootCommand.Flags().BoolVarP(&passThroughFlag, "passthrough", "", false, "if true just passthrough all H264 NAL-Units")
 	rootCommand.Flags().StringVarP(&customCAFileFlag, "custom-ca", "", "", "custom CA file")
 	rootCommand.Flags().BoolVarP(&insecureSkipVerifyFlag, "insecure", "", false, "if true don't verify remote tls certificates")
+	rootCommand.Flags().BoolVarP(&useH265CodecFlag, "h265", "", false, "If true, expect h265 insteand of h264")
 
 	rootCommand.Execute()
 }
@@ -197,7 +202,6 @@ func rtspClientExample(apiKeyOrGuestlink, rtspConnectURL, apiEndpoint, user,
 
 	clientOptions := []ghost.ClientOption{
 		ghost.WithCustomLogger(&Logger{}),
-		ghost.WithForceH264Codec(),
 		ghost.WithSendOnly(),
 	}
 	if len(customCAFileFlag) > 0 {
@@ -207,6 +211,13 @@ func rtspClientExample(apiKeyOrGuestlink, rtspConnectURL, apiEndpoint, user,
 	if insecureSkipVerifyFlag {
 		clientOptions = append(clientOptions, ghost.WithInsecureSkipVerify())
 	}
+
+	if useH265CodecFlag {
+		clientOptions = append(clientOptions, ghost.WithForceH265Codec())
+	} else {
+		clientOptions = append(clientOptions, ghost.WithForceH264Codec())
+	}
+
 	eyesonClient, err := ghost.NewClient(room.Data, clientOptions...)
 	if err != nil {
 		log.Error().Err(err).Msg("Failed to create eyeson-client")
@@ -228,7 +239,7 @@ func rtspClientExample(apiKeyOrGuestlink, rtspConnectURL, apiEndpoint, user,
 	eyesonClient.SetConnectedHandler(func(connected bool, localVideoTrack ghost.RTPWriter,
 		localAudioTrack ghost.RTPWriter) {
 		log.Info().Msgf("Webrtc connected. Connecting to %s", rtspConnectURL)
-		setupRtspClient(localVideoTrack, rtspConnectURL, rtspTerminatedCh)
+		setupRtspClient(localVideoTrack, rtspConnectURL, rtspTerminatedCh, useH265CodecFlag)
 	})
 
 	if err := eyesonClient.Call(); err != nil {
@@ -253,7 +264,25 @@ func rtspClientExample(apiKeyOrGuestlink, rtspConnectURL, apiEndpoint, user,
 	eyesonClient.TerminateCall()
 }
 
-func forward(nalus [][]byte, encoder *rtph264.Encoder, videoTrack ghost.RTPWriter, rtpTimestamp uint32) error {
+func forwardh265(nalus [][]byte, encoder *rtph265.Encoder, videoTrack ghost.RTPWriter, rtpTimestamp uint32) error {
+	pkts, err := encoder.Encode(nalus)
+	if err != nil {
+		return fmt.Errorf("error while encoding H265: %v", err)
+	}
+
+	for _, pkt := range pkts {
+		// log.Debug().Msgf("Final ts: %d seq: %d len: %d mark: %v", pkt.Timestamp, pkt.SequenceNumber,
+		// 	len(pkt.Payload), pkt.Marker)
+		pkt.Timestamp = rtpTimestamp
+		err = videoTrack.WriteRTP(pkt)
+		if err != nil {
+			return fmt.Errorf("Failed to write h265 sample %v", err)
+		}
+	}
+	return nil
+}
+
+func forwardh264(nalus [][]byte, encoder *rtph264.Encoder, videoTrack ghost.RTPWriter, rtpTimestamp uint32) error {
 	pkts, err := encoder.Encode(nalus)
 	if err != nil {
 		return fmt.Errorf("error while encoding H264: %v", err)
@@ -272,10 +301,7 @@ func forward(nalus [][]byte, encoder *rtph264.Encoder, videoTrack ghost.RTPWrite
 }
 
 func setupRtspClient(videoTrack ghost.RTPWriter, rtspConnectURL string,
-	rtspTerminated chan<- bool) {
-	//
-	// start rtsp-listener
-	//
+	rtspTerminated chan<- bool, codecH265 bool) {
 	go func() {
 
 		// Make ReadBufferSize large enough to fit whole keyframes.
@@ -309,115 +335,255 @@ func setupRtspClient(videoTrack ghost.RTPWriter, rtspConnectURL string,
 
 		log.Debug().Msgf("baseurl: %s", baseURL)
 
+		var fh265 *format.H265
+		mediaH265 := session.FindFormat(&fh265)
+
 		var fh264 *format.H264
 		mediaH264 := session.FindFormat(&fh264)
 
-		if mediaH264 == nil {
-			log.Error().Msg("No h264 media found")
+		if codecH265 && mediaH265 == nil {
+			log.Error().Msg("Expecting h265 codec but no h265 media found")
+			if mediaH264 != nil {
+				log.Error().Msg("Since h264-codec is present, think of starting without --h265 switch")
+			}
 			rtspTerminated <- true
 			return
 		}
 
-		sps := fh264.SPS
-		pps := fh264.PPS
-
-		if !passThroughFlag {
-			if sps == nil || pps == nil {
-				if sps == nil {
-					log.Warn().Msg("SPS not present")
-				}
-				if pps == nil {
-					log.Warn().Msg("PPS not present")
-				}
+		if !codecH265 && mediaH264 == nil {
+			log.Error().Msg("Expecting h264 codec but no h264 media found")
+			if mediaH265 != nil {
+				log.Error().Msg("Since h265-codec is present, think of starting with --h265 switch")
 			}
-		}
-
-		h264Encoder := rtph264.Encoder{
-			PayloadType:    96,
-			PayloadMaxSize: 1200,
-		}
-		h264Encoder.Init()
-
-		// setup RTP->H264 decoder
-		rtpDec := &rtph264.Decoder{}
-		rtpDec.Init()
-
-		// setup a single media
-		_, err = c.Setup(session.BaseURL, mediaH264, 0, 0)
-		if err != nil {
-			log.Error().Err(err).Msg("Failed to start rtsp")
 			rtspTerminated <- true
 			return
 		}
 
-		var lastRTPts uint32
-
-		firstKeyFrame := false
-
-		// buffers nalus for the same timestamp (rtpTimestamp)
-		nalusBuffer := [][]byte{}
-
-		onRTPPacket := func(pkt *rtp.Packet) {
-
-			if lastRTPts != 0 {
-				if pkt.Timestamp < lastRTPts {
-					log.Info().Msg("Warning: Non monotonic ts. Probably a B-Frame, but B-frames not supported.")
-				}
-			}
-
-			// decode H264 NALUs from the RTP packet
-			nalus, err := rtpDec.Decode(pkt)
-			if err != nil {
-				//log.Printf("Decode failed: %s", err)
-				return
-			}
-
-			if !passThroughFlag {
-				for _, nalu := range nalus {
-					typ := rtsph264.NALUType(nalu[0] & 0x1F)
-					// log.Debug().Msgf("type %s:", typ.String())
-					switch typ {
-					case rtsph264.NALUTypeIDR:
-						// prepend keyframe with SPS and PPS
-						nalus = append([][]byte{pps}, nalus...)
-						nalus = append([][]byte{sps}, nalus...)
-						firstKeyFrame = true
-					}
-				}
-
-				// wait for first key-frame
-				if !firstKeyFrame {
-					return
-				}
-			}
-
-			if lastRTPts != pkt.Timestamp {
-				// last frame is complete, so forward nalus and clear the buffer
-				if err := forward(nalusBuffer, &h264Encoder, videoTrack, lastRTPts); err != nil {
-					log.Error().Err(err).Msg("Failed to forward")
-					return
-				}
-				nalusBuffer = [][]byte{}
-			}
-			nalusBuffer = append(nalusBuffer, nalus...)
-			lastRTPts = pkt.Timestamp
+		if codecH265 {
+			setupRtspClientH265(videoTrack, rtspTerminated, fh265, mediaH265, session, &c)
+		} else {
+			setupRtspClientH264(videoTrack, rtspTerminated, fh264, mediaH264, session, &c)
 		}
-
-		c.OnPacketRTP(mediaH264, fh264, onRTPPacket)
-
-		_, err = c.Play(nil)
-		if err != nil {
-			log.Error().Err(err).Msg("Failed to start rtsp")
-			rtspTerminated <- true
-			return
-		}
-
-		// wait until a fatal error
-		if err := c.Wait(); err != nil {
-			log.Error().Err(err).Msg("RTSP finished with err")
-		}
-
-		rtspTerminated <- true
 
 	}()
+}
+
+func setupRtspClientH265(videoTrack ghost.RTPWriter,
+	rtspTerminated chan<- bool, fh265 *format.H265, mediaH265 *description.Media,
+	session *description.Session, c *gortsplib.Client) {
+
+	sps := fh265.SPS
+	pps := fh265.PPS
+	vps := fh265.VPS
+
+	if !passThroughFlag {
+		if sps == nil || pps == nil || vps == nil {
+			if sps == nil {
+				log.Warn().Msg("SPS not present")
+			}
+			if pps == nil {
+				log.Warn().Msg("PPS not present")
+			}
+			if vps == nil {
+				log.Warn().Msg("VPS not present")
+			}
+		}
+	}
+
+	h265Encoder := rtph265.Encoder{
+		PayloadType:    96,
+		PayloadMaxSize: 1200,
+	}
+	h265Encoder.Init()
+
+	// setup RTP->H265 decoder
+	rtpDec := &rtph265.Decoder{}
+	rtpDec.Init()
+
+	// setup a single media
+	_, err := c.Setup(session.BaseURL, mediaH265, 0, 0)
+	if err != nil {
+		log.Error().Err(err).Msg("Failed to start rtsp")
+		rtspTerminated <- true
+		return
+	}
+
+	var lastRTPts uint32
+
+	firstKeyFrame := false
+
+	// buffers nalus for the same timestamp (rtpTimestamp)
+	nalusBuffer := [][]byte{}
+
+	onRTPPacket := func(pkt *rtp.Packet) {
+		//log.Info().Msgf("packet %d", pkt.Timestamp)
+
+		if lastRTPts != 0 {
+			if pkt.Timestamp < lastRTPts {
+				log.Info().Msg("Warning: Non monotonic ts. Probably a B-Frame, but B-frames not supported.")
+			}
+		}
+
+		// decode H265 NALUs from the RTP packet
+		nalus, err := rtpDec.Decode(pkt)
+		if err != nil {
+			//log.Printf("Decode failed: %s", err)
+			return
+		}
+
+		if !passThroughFlag {
+			for _, nalu := range nalus {
+				typ := rtsph265.NALUType((nalu[0] >> 1) & 0x3F)
+				//log.Debug().Msgf("type %s(%d):", typ.String(), typ)
+				switch typ {
+				case rtsph265.NALUType_IDR_N_LP, rtsph265.NALUType_IDR_W_RADL:
+					// prepend keyframe with SPS and PPS
+					nalus = append([][]byte{pps}, nalus...)
+					nalus = append([][]byte{sps}, nalus...)
+					nalus = append([][]byte{vps}, nalus...)
+					firstKeyFrame = true
+				}
+			}
+
+			// wait for first key-frame
+			if !firstKeyFrame {
+				return
+			}
+		}
+
+		if lastRTPts != pkt.Timestamp {
+			// last frame is complete, so forward nalus and clear the buffer
+			if err := forwardh265(nalusBuffer, &h265Encoder, videoTrack, lastRTPts); err != nil {
+				log.Error().Err(err).Msg("Failed to forward")
+				return
+			}
+			nalusBuffer = [][]byte{}
+		}
+		nalusBuffer = append(nalusBuffer, nalus...)
+		lastRTPts = pkt.Timestamp
+	}
+
+	c.OnPacketRTP(mediaH265, fh265, onRTPPacket)
+
+	_, err = c.Play(nil)
+	if err != nil {
+		log.Error().Err(err).Msg("Failed to start rtsp")
+		rtspTerminated <- true
+		return
+	}
+
+	// wait until a fatal error
+	if err := c.Wait(); err != nil {
+		log.Error().Err(err).Msg("RTSP finished with err")
+	}
+
+	rtspTerminated <- true
+
+}
+
+func setupRtspClientH264(videoTrack ghost.RTPWriter,
+	rtspTerminated chan<- bool, fh264 *format.H264, mediaH264 *description.Media,
+	session *description.Session, c *gortsplib.Client) {
+
+	sps := fh264.SPS
+	pps := fh264.PPS
+
+	if !passThroughFlag {
+		if sps == nil || pps == nil {
+			if sps == nil {
+				log.Warn().Msg("SPS not present")
+			}
+			if pps == nil {
+				log.Warn().Msg("PPS not present")
+			}
+		}
+	}
+
+	h264Encoder := rtph264.Encoder{
+		PayloadType:    96,
+		PayloadMaxSize: 1200,
+	}
+	h264Encoder.Init()
+
+	// setup RTP->H264 decoder
+	rtpDec := &rtph264.Decoder{}
+	rtpDec.Init()
+
+	// setup a single media
+	_, err := c.Setup(session.BaseURL, mediaH264, 0, 0)
+	if err != nil {
+		log.Error().Err(err).Msg("Failed to start rtsp")
+		rtspTerminated <- true
+		return
+	}
+
+	var lastRTPts uint32
+
+	firstKeyFrame := false
+
+	// buffers nalus for the same timestamp (rtpTimestamp)
+	nalusBuffer := [][]byte{}
+
+	onRTPPacket := func(pkt *rtp.Packet) {
+
+		if lastRTPts != 0 {
+			if pkt.Timestamp < lastRTPts {
+				log.Info().Msg("Warning: Non monotonic ts. Probably a B-Frame, but B-frames not supported.")
+			}
+		}
+
+		// decode H264 NALUs from the RTP packet
+		nalus, err := rtpDec.Decode(pkt)
+		if err != nil {
+			//log.Printf("Decode failed: %s", err)
+			return
+		}
+
+		if !passThroughFlag {
+			for _, nalu := range nalus {
+				typ := rtsph264.NALUType(nalu[0] & 0x1F)
+				// log.Debug().Msgf("type %s:", typ.String())
+				switch typ {
+				case rtsph264.NALUTypeIDR:
+					// prepend keyframe with SPS and PPS
+					nalus = append([][]byte{pps}, nalus...)
+					nalus = append([][]byte{sps}, nalus...)
+					firstKeyFrame = true
+				}
+			}
+
+			// wait for first key-frame
+			if !firstKeyFrame {
+				return
+			}
+		}
+
+		if lastRTPts != pkt.Timestamp {
+			// last frame is complete, so forward nalus and clear the buffer
+			if err := forwardh264(nalusBuffer, &h264Encoder, videoTrack, lastRTPts); err != nil {
+				log.Error().Err(err).Msg("Failed to forward")
+				return
+			}
+			nalusBuffer = [][]byte{}
+		}
+		nalusBuffer = append(nalusBuffer, nalus...)
+		lastRTPts = pkt.Timestamp
+	}
+
+	c.OnPacketRTP(mediaH264, fh264, onRTPPacket)
+
+	_, err = c.Play(nil)
+	if err != nil {
+		log.Error().Err(err).Msg("Failed to start rtsp")
+		rtspTerminated <- true
+		return
+	}
+
+	// wait until a fatal error
+	if err := c.Wait(); err != nil {
+		log.Error().Err(err).Msg("RTSP finished with err")
+	}
+
+	rtspTerminated <- true
+
 }
